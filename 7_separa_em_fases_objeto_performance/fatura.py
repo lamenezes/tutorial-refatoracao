ddec02c5005bda1c9d3aecb8447ad4ad85636b9e
Luiz Menezes <luiz.menezesf@gmail.com>
2024-10-05 19:31:28 -0300

    Move valor_da() para objeto Performance

--- 7_separa_em_fases_objeto_performance/fatura.py
+++ 7_separa_em_fases_objeto_performance/fatura.py
@@ -18,6 +18,22 @@ class Performance:
     def obra(self):
         return self.obras[self.id_obra]
 
+    def calcula_valor(self):
+        resultado = 0
+        if self.obra["tipo"] == "tragédia":
+            resultado = 40_000
+            if self.espectadores > 30:
+                resultado += 1000 * (self.espectadores - 30)
+        elif self.obra["tipo"] == "comédia":
+            resultado = 30_000
+            if self.espectadores > 20:
+                resultado += 10000 + 500 * (self.espectadores - 20)
+            resultado += 300 * self.espectadores
+        else:
+            raise ValueError(f"Tipo de obra desconhecido {self.obra['tipo']}")
+
+        return resultado
+
 
 @dataclass
 class Fatura:
@@ -36,22 +52,6 @@ def fatura(dados_demonstrativo, obras):
 def renderiza_texto_plano(fatura, obras):
     resultado = f"Recibo para {fatura.cliente}\n"
 
-    def valor_da(performance):
-        resultado = 0
-        if performance.obra["tipo"] == "tragédia":
-            resultado = 40_000
-            if performance.espectadores > 30:
-                resultado += 1000 * (performance.espectadores - 30)
-        elif performance.obra["tipo"] == "comédia":
-            resultado = 30_000
-            if performance.espectadores > 20:
-                resultado += 10000 + 500 * (performance.espectadores - 20)
-            resultado += 300 * performance.espectadores
-        else:
-            raise ValueError(f"Tipo de obra desconhecido {performance.obra['tipo']}")
-
-        return resultado
-
     def creditos_da(performance):
         resultado = max(performance.espectadores - 30, 0)
         # soma um crédito extra para cada dez espectadores de comédia
@@ -69,12 +69,12 @@ def renderiza_texto_plano(fatura, obras):
     def valor_total(performances):
         resultado = 0
         for performance in performances:
-            resultado += valor_da(performance)
+            resultado += performance.calcula_valor()
         return resultado
 
     for performance in fatura.performances:
         # soma créditos por volume
-        resultado += f"  {performance.obra['nome']}: {brl(valor_da(performance)/ 100)} ({performance.espectadores} lugares)\n"
+        resultado += f"  {performance.obra['nome']}: {brl(performance.calcula_valor()/ 100)} ({performance.espectadores} lugares)\n"
 
     valor_total = valor_total(fatura.performances)
     resultado += f"Valor a pagar é de {brl(valor_total / 100)}\n"
