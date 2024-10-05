2c70819af629b7a47f7006f805e38fc336da4999
Luiz Menezes <luiz.menezesf@gmail.com>
2024-10-05 18:57:03 -0300

    Encapsula registro (performance)

--- 7_separa_em_fases_objeto_performance/fatura.py
+++ 7_separa_em_fases_objeto_performance/fatura.py
@@ -2,9 +2,21 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Performance:
+    id_obra: str
+    espectadores: int
+
+
+@dataclass
 class Fatura:
     cliente: str
     performances: list[dict]
+
+    def __post_init__(self):
+        performances = []
+        for performance in self.performances:
+            performances.append(Performance(**performance))
+        self.performances = performances
 
 
 def fatura(dados_demonstrativo, obras):
@@ -16,29 +28,29 @@ def renderiza_texto_plano(fatura, obras):
     resultado = f"Recibo para {fatura.cliente}\n"
 
     def obra_da(performance):
-        return obras[performance["id_obra"]]
+        return obras[performance.id_obra]
 
     def valor_da(performance):
         resultado = 0
         if obra_da(performance)["tipo"] == "tragédia":
             resultado = 40_000
-            if performance["espectadores"] > 30:
-                resultado += 1000 * (performance["espectadores"] - 30)
+            if performance.espectadores > 30:
+                resultado += 1000 * (performance.espectadores - 30)
         elif obra_da(performance)["tipo"] == "comédia":
             resultado = 30_000
-            if performance["espectadores"] > 20:
-                resultado += 10000 + 500 * (performance["espectadores"] - 20)
-            resultado += 300 * performance["espectadores"]
+            if performance.espectadores > 20:
+                resultado += 10000 + 500 * (performance.espectadores - 20)
+            resultado += 300 * performance.espectadores
         else:
             raise ValueError(f"Tipo de obra desconhecido {obra_da(performance)['tipo']}")
 
         return resultado
 
     def creditos_da(performance):
-        resultado = max(performance["espectadores"] - 30, 0)
+        resultado = max(performance.espectadores - 30, 0)
         # soma um crédito extra para cada dez espectadores de comédia
         if obra_da(performance)["tipo"] == "comédia":
-            resultado += performance["espectadores"] // 5
+            resultado += performance.espectadores // 5
         return resultado
 
     def créditos_totais(performances):
@@ -56,7 +68,7 @@ def renderiza_texto_plano(fatura, obras):
 
     for performance in fatura.performances:
         # soma créditos por volume
-        resultado += f"  {obra_da(performance)['nome']}: {brl(valor_da(performance)/ 100)} ({performance['espectadores']} lugares)\n"
+        resultado += f"  {obra_da(performance)['nome']}: {brl(valor_da(performance)/ 100)} ({performance.espectadores} lugares)\n"
 
     valor_total = valor_total(fatura.performances)
     resultado += f"Valor a pagar é de {brl(valor_total / 100)}\n"
