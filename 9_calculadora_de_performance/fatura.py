03e3de87815a70df33f1d090ac22bd74a1222aff
Luiz Menezes <luiz.menezesf@gmail.com>
2024-10-06 12:23:51 -0300

    Adiciona CalculadoraPerformance e a propriedade valor

--- 9_calculadora_de_performance/fatura.py
+++ 9_calculadora_de_performance/fatura.py
@@ -64,3 +64,25 @@ class Performance:
         if self.obra["tipo"] == "comédia":
             resultado += self.espectadores // 5
         return resultado
+
+
+class CalculadoraPerformance:
+    def __init__(self, performance):
+        self.performance = performance
+
+    @property
+    def valor(self):
+        resultado = 0
+        if self.performance.obra["tipo"] == "tragédia":
+            resultado = 40_000
+            if self.performance.espectadores > 30:
+                resultado += 1000 * (self.performance.espectadores - 30)
+        elif self.performance.obra["tipo"] == "comédia":
+            resultado = 30_000
+            if self.performance.espectadores > 20:
+                resultado += 10000 + 500 * (self.performance.espectadores - 20)
+            resultado += 300 * self.performance.espectadores
+        else:
+            raise ValueError(f"Tipo de obra desconhecido {self.performance.obra['tipo']}")
+
+        return resultado
