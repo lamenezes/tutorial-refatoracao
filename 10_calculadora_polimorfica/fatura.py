27c62c4f0b57b99cd06fa984e0aba97b331bed81
Luiz Menezes <luiz.menezesf@gmail.com>
2024-10-06 12:51:21 -0300

    Move CalculadoraPerformance para seu próprio módulo

--- 10_calculadora_polimorfica/fatura.py
+++ 10_calculadora_polimorfica/fatura.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from calculadora import CalculadoraPerformance
 
 
 @dataclass
@@ -47,35 +48,3 @@ class Performance:
         calculadora = CalculadoraPerformance(self)
         self.valor = calculadora.valor
         self.creditos = calculadora.creditos
-
-
-class CalculadoraPerformance:
-    def __init__(self, performance):
-        self.performance = performance
-
-    @property
-    def valor(self):
-        resultado = 0
-        if self.performance.obra["tipo"] == "tragédia":
-            resultado = 40_000
-            if self.performance.espectadores > 30:
-                resultado += 1000 * (self.performance.espectadores - 30)
-        elif self.performance.obra["tipo"] == "comédia":
-            resultado = 30_000
-            if self.performance.espectadores > 20:
-                resultado += 10000 + 500 * (self.performance.espectadores - 20)
-            resultado += 300 * self.performance.espectadores
-        else:
-            raise ValueError(
-                f"Tipo de obra desconhecido {self.performance.obra['tipo']}"
-            )
-
-        return resultado
-
-    @property
-    def creditos(self):
-        resultado = max(self.performance.espectadores - 30, 0)
-        # soma um crédito extra para cada dez espectadores de comédia
-        if self.performance.obra["tipo"] == "comédia":
-            resultado += self.performance.espectadores // 5
-        return resultado
