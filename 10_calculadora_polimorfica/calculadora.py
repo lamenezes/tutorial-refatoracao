6abd74f88caf1a8de1c86ed5619796c978136c43
Luiz Menezes <luiz.menezesf@gmail.com>
2024-10-06 13:02:57 -0300

    Move cálculo de valor da tragédia para CalculadoraTragedia

--- 10_calculadora_polimorfica/calculadora.py
+++ 10_calculadora_polimorfica/calculadora.py
@@ -12,22 +12,7 @@ class CalculadoraPerformance:
 
     @property
     def valor(self):
-        resultado = 0
-        if self.performance.obra["tipo"] == "tragédia":
-            resultado = 40_000
-            if self.performance.espectadores > 30:
-                resultado += 1000 * (self.performance.espectadores - 30)
-        elif self.performance.obra["tipo"] == "comédia":
-            resultado = 30_000
-            if self.performance.espectadores > 20:
-                resultado += 10000 + 500 * (self.performance.espectadores - 20)
-            resultado += 300 * self.performance.espectadores
-        else:
-            raise ValueError(
-                f"Tipo de obra desconhecido {self.performance.obra['tipo']}"
-            )
-
-        return resultado
+        return NotImplementedError()
 
     @property
     def creditos(self):
@@ -48,6 +33,10 @@ class CalculadoraComedia(CalculadoraPerformance):
         return resultado
 
 
-
 class CalculadoraTragedia(CalculadoraPerformance):
-    pass
+    @property
+    def valor(self):
+        resultado = 40_000
+        if self.performance.espectadores > 30:
+            resultado += 1000 * (self.performance.espectadores - 30)
+        return resultado
