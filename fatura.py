5342def12ad351856f6a37e37ef74a313fb9b2fa
Luiz Menezes <luiz.menezes@xapo.com>
2024-10-10 20:56:59 -0300

    Separa for de calculo do valor

--- fatura.py
+++ fatura.py
@@ -6,9 +6,11 @@ def fatura(dados_demonstrativo, obras):
     for performance in dados_demonstrativo["performances"]:
         performance["obra"] = obras[performance["id_obra"]]
         total_créditos += calcula_creditos(performance)
+
+    for performance in dados_demonstrativo["performances"]:
         valor_atual = calcula_valor(performance)
-        resultado += f"  {performance['obra']['nome']}: {formata_brl(valor_atual)} ({performance['espectadores']} lugares)\n"
         valor_total += valor_atual
+        resultado += f"  {performance['obra']['nome']}: {formata_brl(valor_atual)} ({performance['espectadores']} lugares)\n"
 
     resultado += f"Valor a pagar é de {formata_brl(valor_total)}\n"
     resultado += f"Você ganhou {total_créditos} créditos\n"
