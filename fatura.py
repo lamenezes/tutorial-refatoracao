e663bc1c3fbeaafcd73d07627cba5a0d06b48b76
Luiz Menezes <luiz.menezes@xapo.com>
2024-10-10 20:25:19 -0300

    Move função calcula_valor() pra fora de fatura()

--- fatura.py
+++ fatura.py
@@ -6,21 +6,6 @@ def fatura(dados_demonstrativo, obras):
     for performance in dados_demonstrativo["performances"]:
         obra = obras[performance["id_obra"]]
 
-        def calcula_valor(performance, obra):
-            valor_atual = 0
-            if obra["tipo"] == "tragédia":
-                valor_atual = 40_000
-                if performance["espectadores"] > 30:
-                    valor_atual += 1000 * (performance["espectadores"] - 30)
-            elif obra["tipo"] == "comédia":
-                valor_atual = 30_000
-                if performance["espectadores"] > 20:
-                    valor_atual += 10000 + 500 * (performance["espectadores"] - 20)
-                valor_atual += 300 * performance["espectadores"]
-            else:
-                raise ValueError(f"Tipo de obra desconhecido {obra['tipo']}")
-            return valor_atual
-
         # soma créditos por volume
         total_créditos += max(performance["espectadores"] - 30, 0)
         # soma um crédito extra para cada dez espectadores de comédia
@@ -34,3 +19,19 @@ def fatura(dados_demonstrativo, obras):
     resultado += f"Valor a pagar é de R$ {valor_total / 100:.2f}\n"
     resultado += f"Você ganhou {total_créditos} créditos\n"
     return resultado
+
+
+def calcula_valor(performance, obra):
+    valor_atual = 0
+    if obra["tipo"] == "tragédia":
+        valor_atual = 40_000
+        if performance["espectadores"] > 30:
+            valor_atual += 1000 * (performance["espectadores"] - 30)
+    elif obra["tipo"] == "comédia":
+        valor_atual = 30_000
+        if performance["espectadores"] > 20:
+            valor_atual += 10000 + 500 * (performance["espectadores"] - 20)
+        valor_atual += 300 * performance["espectadores"]
+    else:
+        raise ValueError(f"Tipo de obra desconhecido {obra['tipo']}")
+    return valor_atual
