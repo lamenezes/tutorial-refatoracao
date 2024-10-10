c3b09d028b0c4e626288c069421ac60a175f3a31
Luiz Menezes <luiz.menezes@xapo.com>
2024-10-10 20:36:36 -0300

    Remove parametro obra da função calcula_valor()

--- fatura.py
+++ fatura.py
@@ -4,16 +4,16 @@ def fatura(dados_demonstrativo, obras):
     resultado = f"Recibo para {dados_demonstrativo['cliente']}\n"
 
     for performance in dados_demonstrativo["performances"]:
-        obra = obras[performance["id_obra"]]
+        performance["obra"] = obras[performance["id_obra"]]
 
         # soma créditos por volume
         total_créditos += max(performance["espectadores"] - 30, 0)
         # soma um crédito extra para cada dez espectadores de comédia
-        if obra["tipo"] == "comédia":
+        if performance["obra"]["tipo"] == "comédia":
             total_créditos += performance["espectadores"] // 5
 
-        valor_atual = calcula_valor(performance, obra)
-        resultado += f"  {obra['nome']}: R$ {valor_atual / 100:.2f} ({performance['espectadores']} lugares)\n"
+        valor_atual = calcula_valor(performance)
+        resultado += f"  {performance['obra']['nome']}: R$ {valor_atual / 100:.2f} ({performance['espectadores']} lugares)\n"
         valor_total += valor_atual
 
     resultado += f"Valor a pagar é de R$ {valor_total / 100:.2f}\n"
@@ -21,17 +21,17 @@ def fatura(dados_demonstrativo, obras):
     return resultado
 
 
-def calcula_valor(performance, obra):
+def calcula_valor(performance):
     valor_atual = 0
-    if obra["tipo"] == "tragédia":
+    if performance["obra"]["tipo"] == "tragédia":
         valor_atual = 40_000
         if performance["espectadores"] > 30:
             valor_atual += 1000 * (performance["espectadores"] - 30)
-    elif obra["tipo"] == "comédia":
+    elif performance["obra"]["tipo"] == "comédia":
         valor_atual = 30_000
         if performance["espectadores"] > 20:
             valor_atual += 10000 + 500 * (performance["espectadores"] - 20)
         valor_atual += 300 * performance["espectadores"]
     else:
-        raise ValueError(f"Tipo de obra desconhecido {obra['tipo']}")
+        raise ValueError(f"Tipo de obra desconhecido {performance['obra']['tipo']}")
     return valor_atual
