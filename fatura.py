11385c93a28f6f434675aac4e92dac6808e5ac10
Luiz Menezes <luiz.menezes@xapo.com>
2024-10-10 22:28:22 -0300

    Joga funções para suas respectivas classes

--- fatura.py
+++ fatura.py
@@ -13,11 +13,10 @@ def renderiza_texto_plano(fatura, obras):
     resultado = f"Recibo para {fatura.cliente}\n"
 
     for performance in fatura.performances:
-        valor_atual = calcula_valor(performance)
-        resultado += f"  {performance.obra['nome']}: {formata_brl(valor_atual)} ({performance.espectadores} lugares)\n"
+        resultado += f"  {performance.obra['nome']}: {formata_brl(performance.calcula_valor())} ({performance.espectadores} lugares)\n"
 
-    resultado += f"Valor a pagar é de {formata_brl(calcula_valor_total(fatura))}\n"
-    resultado += f"Você ganhou {calcula_creditos_totais(fatura)} créditos\n"
+    resultado += f"Valor a pagar é de {formata_brl(fatura.calcula_valor_total())}\n"
+    resultado += f"Você ganhou {fatura.calcula_creditos_totais()} créditos\n"
     return resultado
 
 
@@ -25,6 +24,18 @@ def renderiza_texto_plano(fatura, obras):
 class Fatura:
     cliente: str
     performances: list['Performance']
+
+    def calcula_creditos_totais(self):
+        total_créditos = 0
+        for performance in self.performances:
+            total_créditos += performance.calcula_creditos()
+        return total_créditos
+
+    def calcula_valor_total(self):
+        valor_total = 0
+        for performance in self.performances:
+            valor_total += performance.calcula_valor()
+        return valor_total
 
 
 @dataclass
@@ -40,45 +51,29 @@ class Performance:
             performances.append(performance)
         return performances
 
+    def calcula_valor(self):
+        valor_atual = 0
+        if self.obra["tipo"] == "tragédia":
+            valor_atual = 40_000
+            if self.espectadores > 30:
+                valor_atual += 1000 * (self.espectadores - 30)
+        elif self.obra["tipo"] == "comédia":
+            valor_atual = 30_000
+            if self.espectadores > 20:
+                valor_atual += 10000 + 500 * (self.espectadores - 20)
+            valor_atual += 300 * self.espectadores
+        else:
+            raise ValueError(f"Tipo de obra desconhecido {self.obra['tipo']}")
+        return valor_atual
 
-def calcula_creditos_totais(fatura):
-    total_créditos = 0
-    for performance in fatura.performances:
-        total_créditos += calcula_creditos(performance)
-    return total_créditos
-
-
-def calcula_valor_total(fatura):
-    valor_total = 0
-    for performance in fatura.performances:
-        valor_total += calcula_valor(performance)
-    return valor_total
-
-
-def calcula_valor(performance):
-    valor_atual = 0
-    if performance.obra["tipo"] == "tragédia":
-        valor_atual = 40_000
-        if performance.espectadores > 30:
-            valor_atual += 1000 * (performance.espectadores - 30)
-    elif performance.obra["tipo"] == "comédia":
-        valor_atual = 30_000
-        if performance.espectadores > 20:
-            valor_atual += 10000 + 500 * (performance.espectadores - 20)
-        valor_atual += 300 * performance.espectadores
-    else:
-        raise ValueError(f"Tipo de obra desconhecido {performance.obra['tipo']}")
-    return valor_atual
-
-
-def calcula_creditos(performance):
-    total_créditos = 0
-    # soma créditos por volume
-    total_créditos += max(performance.espectadores - 30, 0)
-    # soma um crédito extra para cada dez espectadores de comédia
-    if performance.obra["tipo"] == "comédia":
-        total_créditos += performance.espectadores // 5
-    return total_créditos
+    def calcula_creditos(self):
+        total_créditos = 0
+        # soma créditos por volume
+        total_créditos += max(self.espectadores - 30, 0)
+        # soma um crédito extra para cada dez espectadores de comédia
+        if self.obra["tipo"] == "comédia":
+            total_créditos += self.espectadores // 5
+        return total_créditos
 
 
 def formata_brl(valor):
