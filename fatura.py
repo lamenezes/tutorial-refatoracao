199956f487b4d6854245ff175712a6568905f7ce
Luiz Menezes <luiz.menezes@xapo.com>
2024-10-10 23:06:41 -0300

    Cria arquivo calculadora

--- fatura.py
+++ fatura.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from calculadora import cria_calculadora
 
 
 @dataclass
@@ -35,43 +36,3 @@ class Performance:
     def __post_init__(self):
         self.valor = cria_calculadora(self).valor
         self.creditos = cria_calculadora(self).creditos
-
-
-def cria_calculadora(performance):
-    if performance.obra["tipo"] == "tragédia":
-        return CalculadoraTragedia(performance)
-    elif performance.obra["tipo"] == "comédia":
-        return CalculadoraComedia(performance)
-    raise ValueError(f"Tipo de obra desconhecido {performance.obra['tipo']}")
-
-
-class CalculadoraPerformance:
-    def __init__(self, performance):
-        self.performance = performance
-        self.valor = self.calcula_valor()
-        self.creditos = self.calcula_creditos()
-
-    def calcula_creditos(self):
-        total_créditos = 0
-        total_créditos += max(self.performance.espectadores - 30, 0)
-        # soma um crédito extra para cada dez espectadores de comédia
-        if self.performance.obra["tipo"] == "comédia":
-            total_créditos += self.performance.espectadores // 5
-        return total_créditos
-
-
-class CalculadoraComedia(CalculadoraPerformance):
-    def calcula_valor(self):
-        valor_atual = 30_000
-        if self.performance.espectadores > 20:
-            valor_atual += 10000 + 500 * (self.performance.espectadores - 20)
-        valor_atual += 300 * self.performance.espectadores
-        return valor_atual
-
-
-class CalculadoraTragedia(CalculadoraPerformance):
-    def calcula_valor(self):
-        valor_atual = 40_000
-        if self.performance.espectadores > 30:
-            valor_atual += 1000 * (self.performance.espectadores - 30)
-        return valor_atual
